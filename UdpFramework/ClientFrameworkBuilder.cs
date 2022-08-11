ab0b9d6b09c90d35eca9c30ddf0520dba5ce8b5c
VasiliyKolihalov <vasiliykolihalov@gmail.com>
2022-08-11 20:11:09 +0300

    Refactoring

---
+++ UdpFramework/ClientFrameworkBuilder.cs
@@ -0,0 +1,42 @@
+﻿using LiteNetLib;
+using Microsoft.Extensions.Logging;
+
+namespace Kolyhalov.UdpFramework;
+
+public class ClientFrameworkBuilder
+{
+    public string Address { get; init; } = null!;
+    public Port Port { get; init; } = null!;
+    public Framerate Framerate { get; set; } = null!;
+    public ILogger? Logger { get; set; }
+
+    public ClientUdpFramework Build()
+    {
+        if (Address == null)
+            throw new UdpFrameworkException("Address cannot be null");
+
+        if (Port == null)
+            throw new UdpFrameworkException("Port cannot be null");
+
+        if (Framerate == null)
+            throw new UdpFrameworkException("Framerate cannot be null");
+
+        if (Logger == null)
+        {
+            ILoggerFactory loggerFactory = LoggerFactory.Create(builder => { builder.AddConsole(); });
+            Logger = loggerFactory.CreateLogger<UdpFramework>();
+        }
+
+        var clientConfiguration = new ClientConfiguration(
+            Address,
+            Port.UdpPort,
+            string.Empty, //Todo control version 
+            Framerate.ServerFramerate);
+
+        return new ClientUdpFramework(clientConfiguration,
+            Logger,
+            new EndpointsStorage(),
+            new EndpointsInvoker(),
+            new EventBasedNetListener());
+    }
+}
---
+++ UdpFramework/ServerFrameworkBuilder.cs
@@ -0,0 +1,42 @@
+﻿using LiteNetLib;
+using Microsoft.Extensions.Logging;
+
+namespace Kolyhalov.UdpFramework;
+
+public class ServerFrameworkBuilder
+{
+    public Port Port { get; init; } = null!;
+    public Framerate Framerate { get; init; } = null!;
+    public int MaxPeersCount { get; init; }
+    public ILogger? Logger { get; set; }
+
+    public ServerUdpFramework Build()
+    {
+        if (Port == null)
+            throw new UdpFrameworkException("Port cannot be null");
+
+        if (Framerate == null)
+            throw new UdpFrameworkException("Framerate cannot be null");
+
+        if (MaxPeersCount is 0 or < 0)
+            throw new UdpFrameworkException("MaxPeersCount cannot be zero or bellow");
+
+        if (Logger == null)
+        {
+            ILoggerFactory loggerFactory = LoggerFactory.Create(builder => { builder.AddConsole(); });
+            Logger = loggerFactory.CreateLogger<UdpFramework>();
+        }
+
+        var serverConfiguration = new ServerConfiguration(
+            Port.UdpPort,
+            string.Empty, //Todo control version
+            Framerate.ServerFramerate,
+            Framerate.ServerFramerate);
+
+        return new ServerUdpFramework(serverConfiguration,
+            Logger,
+            new EndpointsStorage(),
+            new EndpointsInvoker(),
+            new EventBasedNetListener());
+    }
+}
