c50725b89b76a4d8735d386fe3a2bc55fd931f44
Nikita Trubkin <trubkin.nikita@yandex.ru>
2022-10-19 13:55:38 +0300

    #94 Add json converters and SchemaAttribute

---
+++ FatNetLib/PackageSchemaConverter.cs
@@ -0,0 +1,56 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace Kolyhalov.FatNetLib;
+
+public class PackageSchemaConverter : JsonConverter<PackageSchema>
+{
+    public override void WriteJson(JsonWriter writer, PackageSchema? value, JsonSerializer serializer)
+    {
+        if (value == null)
+        {
+            writer.WriteNull();
+            return;
+        }
+        writer.WriteStartObject();
+        foreach (KeyValuePair<string,Type> keyTypePair in value)
+        {
+            writer.WritePropertyName(keyTypePair.Key);
+            JsonConverter[] jsonConverters = serializer.Converters.ToArray();
+            JToken.FromObject(keyTypePair.Value, serializer)
+                .WriteTo(writer, jsonConverters);
+        }
+        writer.WriteEndObject();
+    }
+
+    // Todo: make a correct state machine
+    public override PackageSchema? ReadJson(JsonReader reader, Type objectType, PackageSchema? existingValue, bool hasExistingValue,
+        JsonSerializer serializer)
+    {
+        var schema = new PackageSchema();
+        string key = null!;
+        do
+        {
+            switch (reader.TokenType)
+            {
+                case JsonToken.StartObject:
+                    break;
+                case JsonToken.PropertyName:
+                    key = (reader.Value as string)!;
+                    break;
+                case JsonToken.String:
+                    schema[key] = JToken.ReadFrom(reader)
+                        .ToObject<Type>(serializer)!;
+                    break;
+                case JsonToken.EndObject:
+                    return schema;
+                case JsonToken.Null:
+                    return null;
+                default:
+                    throw new FatNetLibException($"Unexpected token {reader.TokenType}");
+            }
+        } while (reader.Read());
+
+        throw new FatNetLibException("Unexpected reader end");
+    }
+}
---
+++ FatNetLib/TypeConverter.cs
@@ -0,0 +1,26 @@
+﻿using Newtonsoft.Json;
+
+namespace Kolyhalov.FatNetLib;
+
+public class TypeConverter : JsonConverter<Type>
+{
+    public override void WriteJson(JsonWriter writer, Type? value, JsonSerializer serializer)
+    {
+        if (value == null)
+        {
+            writer.WriteNull();
+            return;
+        }
+
+        writer.WriteValue(value.FullName + "," + value.Assembly.GetName().Name);
+    }
+
+    public override Type? ReadJson(JsonReader reader, Type objectType, Type? existingValue, bool hasExistingValue,
+        JsonSerializer serializer)
+    {
+        var value = (string?)reader.Value;
+        return value == null
+            ? null
+            : Type.GetType(value, throwOnError: true);
+    }
+}
